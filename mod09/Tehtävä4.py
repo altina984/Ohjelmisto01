70ecc19aafe2e083d85f534b614532dae996e840
altina984 <altinak@metropolia.fi>
2026-09-25 13:49:50 +0300

    Remove car simulation and output logic
    Removed the simulation loop and related code for car objects.

--- mod09/Tehtävä4.py
+++ mod09/Tehtävä4.py
@@ -17,21 +17,3 @@ class Auto:
     def kulje(self, tunnit):
        self.kuljettumatka = self.kuljettumatka + self.nykynopeus * tunnit
      
-autot = []
-
-for i in range(10):
-   rekisteritunnus = "ABC-" + str(i + 1)
-   huippunopeus = random.randint(100, 200)
-   auto = Auto(rekisteritunnus, huippunopeus)
-   autot.append(auto)
-while True:
-   for auto in autot:
-      muutos = random.randint(-10, 15)
-      auto.kiihdytä(muutos)
-      auto.kulje(1)
-
-   if any(auto.kuljettumatka >= 10000 for auto in autot):
-     break
-print("Rekisteri\tHuippunopeus\tNykynopeus\tKuljettu matka")
-for auto in autot:
-      print(f"{auto.rekisteritunnus}\t\t{auto.huippunopeus}\t\t{auto.nykynopeus}\t\t{auto.kuljettumatka}")
